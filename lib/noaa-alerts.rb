bba5fa9418ee5d49069ab03d68468e0c0a2c87da
Cameron Cundiff <ckundo@gmail.com>
2012-04-20 16:05:43 -0400

    Refactored method names and Alerts module behaviors.

--- lib/noaa-alerts.rb
+++ lib/noaa-alerts.rb
@@ -3,22 +3,23 @@ require "httparty"
 
 module Noaa
   module Alerts
-    def self.fetch_and_create(state_abbr)
+    def self.find_by_state(state_abbr)
       messages = []
       alerts = []
-      entries = fetch_entries(state_abbr)
 
-      entries.each do |entry|
-        messages << fetch_alert_from_entry(entry)
+      catalog = message_catalog_for_state(state_abbr)
+      catalog.each do |entry|
+        messages << message_from_catalog_entry(entry)
       end
 
       messages.each do |message|
         alerts << Alert.new(message)
       end
+
       return alerts
     end
 
-    def self.fetch_entries(state_abbr)
+    def self.message_catalog_for_state(state_abbr)
       url = "http://alerts.weather.gov/cap/#{state_abbr}.php?x=0"
       response = HTTParty.get(url, :format => :xml).parsed_response
       entries = response.fetch("feed").fetch("entry")
@@ -26,25 +27,31 @@ module Noaa
       return entries
     end
 
-    def self.fetch_alert_from_entry(entry)
+    def self.message_from_catalog_entry(entry)
       url = entry.fetch('id')
       response = HTTParty.get(url, :format => :xml).parsed_response
       response.fetch('alert')
     end
 
     class Alert
-      attr_reader :description, :latitude, :longitude
+      attr_reader :description, :latitude, :longitude, :areas
 
       def initialize(message)
         @description = message.fetch('info').fetch('description')
-        coords = extract_location(message)
+
+        coords = extract_coordinates(message)
         @latitude = coords.fetch(:latitude)
         @longitude = coords.fetch(:longitude)
+
+        @areas = message.fetch('info')
+          .fetch('area')
+          .fetch('areaDesc')
+          .split(': ')
       end
 
-      def extract_location(message)
-        longitude = 0.0
+      def extract_coordinates(message)
         latitude = 0.0
+        longitude = 0.0 
         return { :latitude => latitude, :longitude => longitude }
       end
     end
--- spec/noaa_alerts_spec.rb
+++ spec/noaa_alerts_spec.rb
@@ -1,7 +1,7 @@
 require 'noaa-alerts'
 
 describe Noaa::Alerts do
-  let(:alerts) { Noaa::Alerts.fetch_and_create("ny") }
+  let(:alerts) { Noaa::Alerts.find_by_state("ny") }
 
   describe '.fetch_and_create' do
     it 'returns alerts' do
@@ -13,12 +13,20 @@ describe Noaa::Alerts do
     let(:alert) { alerts.first }
     
     it 'has a description' do
+      alert.description.should be_an_instance_of String
       alert.description.should_not be_empty
     end
     
-    it 'has coordinates' do
+    it 'has a list of affected areas' do
+      alert.areas.should be_an_instance_of Array
+      alert.areas.should_not be_empty
+    end
+
+    xit 'has coordinates' do
       alert.latitude.should_not be_nil
       alert.longitude.should_not be_nil
     end
+    
+    xit 'has geographic bounds'
   end
 end
